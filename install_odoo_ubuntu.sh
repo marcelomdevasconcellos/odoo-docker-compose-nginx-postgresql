7907f33fb5f52c4e301f5d29c59d9684eff5da27
Marcelo Vasconcellos <marcelomdevasconcellos@gmail.com>
2025-06-05 19:06:10 -0400

    Include install docker and uninstall nginx

--- install_odoo_ubuntu.sh
+++ install_odoo_ubuntu.sh
@@ -38,7 +38,7 @@ OE_SUPERADMIN="admin"
 GENERATE_RANDOM_PASSWORD="True"
 OE_CONFIG="${OE_USER}-server"
 # Set the website name
-WEBSITE_NAME="cestinha.marcelovasconcellos.com.br"
+WEBSITE_NAME="odoo.marcelovasconcellos.com.br"
 # Set the default Odoo longpolling port (you still have to use -c /etc/odoo-server.conf for example to use this.)
 LONGPOLLING_PORT="8072"
 # Set to "True" to install certbot and have ssl enabled, "False" to use http
